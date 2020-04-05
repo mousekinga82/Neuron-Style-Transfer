Fix the bug in np.square in NST_util. using dytpe = 'float32' instead to prevent overflow

NST.py
@@ -18,17 +18,23 @@ import matplotlib.pyplot as plt
 from matplotlib.pyplot import imshow
 from NST_util import *
 
-learning_rate = 2.0
+learning_rate = 2
 iter_num = 200
-print_rate = 10
-save_pc_prefix = ""
+print_rate = 25
+save_pc_prefix = "T6"
 
-#Load Content/Style image and then pre-prosessing
-content_image = plt.imread("images/louvre_small.jpg")
+#Load Content image and then pre-prosessing
+#content_image = plt.imread("images/louvre.jpg")
+content_image = resize_image("images/persian_cat.jpg", CONFIG.IMAGE_HEIGHT, CONFIG.IMAGE_WIDTH)
 content_image = reshape_and_normalize_image(content_image)
+#Save content photo H, W to CONFIG (Enable for arbitary size the same as content image)
+#CONFIG.set_content_imageHW(content_image.shape[1], content_image.shape[2])
 
-style_image = plt.imread("images/monet.jpg")
+#Load & resize the sytle image to be the same as content image
+style_image = resize_image("images/VanGogh_2.jpg", CONFIG.IMAGE_HEIGHT, CONFIG.IMAGE_WIDTH)
+#Pre-prosessing style image
 style_image = reshape_and_normalize_image(style_image)
+
 #Generate input image with noise
 generated_image = generate_noise_image(content_image)
 
@@ -36,10 +42,10 @@ generated_image = generate_noise_image(content_image)
 tf.reset_default_graph()
 model = load_vgg_model("pretrained-model/imagenet-vgg-verydeep-19.mat")
 
-#Get the total cost
+#Get the total cost..
 J_content = compute_content_cost(model, content_image, 'conv4_2')
 J_style = compute_style_cost(model, style_image)
-J_total = total_cost(J_content, J_style)
+J_total = total_cost(J_content, J_style, alpha=1000, beta=1)
 
 #Define optimizer
 optimizer = tf.train.AdamOptimizer(learning_rate)
@@ -54,6 +60,7 @@ with tf.Session() as sess:
         sess.run(train_step)
         if i%print_rate == 0:
             Jt, Jc, Js, generated_image = sess.run([J_total, J_content, J_style, model['input']])
+            J_ts = J_style
             print("Iteraiton " + str(i) + ":")
             print("total cost = " + str(Jt))
             print("content cost = " + str(Jc))
@@ -62,3 +69,18 @@ with tf.Session() as sess:
             save_image("output/" + save_pc_prefix + "_" + str(i) + ".png", generated_image)
 #save final image
 save_image('output/' + save_pc_prefix + "_final.jpg", generated_image)
+
+if (4 > 1):
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        sess.run(model['input'].assign(generated_image))
+        a_G1, a_G2 = sess.run([model['conv1_1'], model['conv2_1']])
+        sess.run(model['input'].assign(style_image))
+        a_S1, a_S2 = sess.run([model['conv1_1'], model['conv2_1']])
+        m, n_H, n_W, n_C = a_G1.shape
+        s_cost1 = 0.5 * tf.reduce_sum(tf.square(tf.subtract(a_S1, a_G1)))/(4 * np.square(n_H * n_W * n_C).astype('float32'))
+        m, n_H, n_W, n_C = a_G2.shape
+        s_cost2 = 0.5 * tf.reduce_sum(tf.square(tf.subtract(a_S2, a_G2)))/(4 * np.square(n_H * n_W * n_C).astype('float32'))
+        s_cost =  40 * (s_cost1 + s_cost2)
+        sa1, sa2 = sess.run([s_cost1, s_cost2])
+        J__ts = sess.run(J_ts)

NST_util.py
@@ -10,21 +10,29 @@ import scipy.io
 import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image, ImageOps
 
 class CONFIG:
-    IMAGE_WIDTH = 400
-    IMAGE_HEIGHT = 300
+    IMAGE_HEIGHT = 395
+    IMAGE_WIDTH = 700
     COLOR_CHANNELS = 3
     NOISE_RATIO = 0.6
     VGG_MODEL_PATH = 'pretrained-model/imagenet-vgg-verydeep-19.mat'
     MEANS = np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3)) 
     #Define layer used for style
     STYLE_LAYERS = [
-    ('conv1_1',0.2),
-    ('conv2_1',0.2),
-    ('conv3_1',0.2),
-    ('conv4_1',0.2),
-    ('conv5_1',0.2)]
+    ('conv1_1',0.1),
+    ('conv2_1',0.3),
+    ('conv3_1',0.3),
+    ('conv4_1',0.3)]
+    #('conv5_1',0.0)]
+    
+    @classmethod
+    def set_content_imageHW(cls, H, W):
+        cls.IMAGE_HEIGHT, cls.IMAGE_WIDTH = (H, W)
+        
+def pp():
+    print(CONFIG.IMAGE_HEIGHT, CONFIG.IMAGE_WIDTH)
 
 def load_vgg_model(path):
     """
@@ -188,7 +196,6 @@ def compute_content_cost(model, content_image_pre, content_layer):
         a_C = sess.run(out)
         #a_G is stll an un-evaluated tensor  
         a_G = out
-    
     #Retrieve dimensoins 
     m, n_H, n_W, n_C = a_G.get_shape().as_list()
     #Reshape a_C & a_G
@@ -208,7 +215,7 @@ def gram_matrix(A):
     GA -- Gram matrix of A, of shape (n_C, n_C)
     """
     n_C, _ = A.get_shape().as_list()
-    GA = tf.matmul(A, tf.transpose(A, perm = [1, 0]))  
+    GA = tf.matmul(A, tf.transpose(A, perm = [1, 0]))
     
     return GA
 
@@ -230,7 +237,7 @@ def compute_layer_style_cost(a_S, a_G):
     GS = gram_matrix(a_S)
     GG = gram_matrix(a_G)
     #Compute the cost
-    J_style_layer = tf.reduce_sum(tf.square(tf.subtract(GS, GG)))/(4 * np.square(n_H * n_W * n_C).astype('float32'))
+    J_style_layer = tf.reduce_sum(tf.square(tf.subtract(GS, GG)))/(4 * np.square(n_H * n_W * n_C, dtype = 'float32'))
     
     return J_style_layer
 
@@ -303,3 +310,11 @@ def save_image(path, image):
     #clip & save image
     image = np.clip(image[0], 0, 255).astype('uint8')
     plt.imsave(path, image)
+    
+def resize_image(path, H, W):
+    """
+    Resize an image to an arbitrary size 
+    """
+    original_image = Image.open(path)
+    fit_and_resized_image = ImageOps.fit(original_image, (W, H), Image.ANTIALIAS)
+    return np.array(fit_and_resized_image)